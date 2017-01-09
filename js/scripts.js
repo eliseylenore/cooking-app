bcd5639429d69e7cb7a70b790e762dedde5fb51e
Chad Durkin and Elise St Hilaire <chad@>
2017-01-09 11:55:54 -0800

    Added a pints to quarts calc

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,35 @@
 var cupsToOunces = function() {
   var cups = prompt("How many cups would you like to convert?");
   var ounces = (cups * 8).toFixed(2);
-  alert(cups + " cups converts to " + ounces + " ounces")
+  alert(cups + " cups converts into " + ounces + " ounces")
 }
-
 
 var gallonsToLiters = function() {
   var gallons = prompt("How many gallons would you like to convert?");
   var liters = (gallons * 3.78541).toFixed(2);
-  alert(gallons + " gallons converts to " + liters + " liters");
+  alert(gallons + " gallons converts into " + liters + " liters");
+}
+
+var pintsToQuarts = function() {
+  var pints = prompt("How many pints would you like to convert?");
+  var quarts = (pints / 2).toFixed(2);
+  alert(pints + " pints converts into " + quarts + " quarts");
 }
 
 
 
-var option = prompt("Which calculator do you want to use? cups or gallons");
+var option = prompt("Which calculator do you want to use? cups, gallons, or pints");
 
 option = option.toLowerCase();
 
 if(option === "cups") {
-cupsToOunces();
+  cupsToOunces();
 }
 
-else if(option === "gallons") {
+else if (option === "gallons") {
   gallonsToLiters();
+}
+
+else if(option === "pints") {
+  pintsToQuarts();
 }

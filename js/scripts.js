6fb6f2ac92686aa7a87c17d192b297f00ed70d3a
Chad Durkin and Elise St Hilaire <elise@>
2017-01-09 11:48:20 -0800

    Added if statement for options

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
+var cupsToOunces = function() {
+  var cups = prompt("How many cups would you like to convert?");
+  var ounces = (cups * 8).toFixed(2);
+  alert(cups + " cups converts to " + ounces + " ounces")
+}
+
+
 var gallonsToLiters = function() {
-  var gallons = prompt("How many gallons would you like to convert?")
+  var gallons = prompt("How many gallons would you like to convert?");
   var liters = (gallons * 3.78541).toFixed(2);
   alert(gallons + " gallons converts to " + liters + " liters");
 }
 
-gallonsToLiters();
+
+
+var option = prompt("Which calculator do you want to use? cups or gallons");
+
+option = option.toLowerCase();
+
+if(option === "cups") {
+cupsToOunces();
+}
+
+else if(option === "gallons") {
+  gallonsToLiters();
+}
